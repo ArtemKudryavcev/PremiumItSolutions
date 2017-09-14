b6e242deb843fc92b827fac3ad01166d63fec84e
artem.kudryavtsev <atrium91mail@mail.ru>
2017-09-14 11:44:50 +0300

    последние изменения 14/09

--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -12,7 +12,7 @@ public class DriverFactory {
 
     public static WebDriver getObject() throws Exception {
         if (webDriver == null) {
-            System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", "src/main/resources/driver/chromedriver.exe");
             webDriver = new ChromeDriver();
             WebDriverRunner.setWebDriver(webDriver); // установка драйвера для селенида
             Configuration.timeout = 45000;           // установка тайм-аута для селенида
